7424c9d88f1a469effe3d785e7721a2c5e554b6a
Anthony Tleiji <anthonytleiji@gmail.com>
2024-01-11 18:21:46 +0200

    Add Navbar Add Footer Add Metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import JobFilterSidebar from "@/components/job-filter-sidebar";
 import JobResults from "@/components/job-results";
 import H1 from "@/components/ui/h1";
 import { JobFilterType } from "@/lib/validation";
+import { Metadata } from "next";
+import { title } from "process";
 
 interface HomeProps {
   searchParams: {
@@ -11,6 +13,32 @@ interface HomeProps {
     remote?: string;
   };
 }
+
+const getTitle = ({ query, type, location, remote }: JobFilterType) => {
+  const titlePrefix = query
+    ? `${query} jobs`
+    : type
+      ? `${type} jobs`
+      : remote
+        ? "Remote jobs"
+        : "All jobs";
+  const titleSuffix = location ? ` in ${location}` : "";
+
+  return `${titlePrefix}${titleSuffix}`;
+};
+
+export const generateMetadata = ({
+  searchParams: { query, type, location, remote },
+}: HomeProps): Metadata => {
+  return {
+    title: `${getTitle({
+      query,
+      type,
+      location,
+      remote: remote === "true",
+    })} | Easy Jobs`,
+  };
+};
 
 export default async function Home({
   searchParams: { query, type, location, remote },
@@ -25,7 +53,7 @@ export default async function Home({
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
-        <H1>Developer Jobs</H1>
+        <H1>{getTitle(filterValues)}</H1>
         <p>Find your next job.</p>
       </div>
       <section className="flex flex-col gap-4 md:flex-row">
